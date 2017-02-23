78fa04a6cce5266cdd133425cccabe348a989439
agis <agis@inarts.co.id>
2017-02-23 12:43:41 +0700

    23
    23

---
+++ pic/ligthBulb.js
@@ -0,0 +1,10 @@
+/**
+ * Created by PROGRAMMER on 23/02/2017.
+ */
+function bulbOn() {
+    document.getElementById('balon').src='pic_bulbon.gif';
+}
+
+function bulbOff() {
+    document.getElementById('balon').src='pic_bulboff.gif';
+}
